c0bb4a50ea1e6ac1d7dec577ded38ee3c9c08657
TheMfrancis <129088983+TheMfrancis@users.noreply.github.com>
2024-04-07 02:11:16 +1000

    Update helper.py

--- part_a/search/helper.py
+++ part_a/search/helper.py
@@ -1,29 +1,9 @@
 from enum import Enum
-from .core import PlayerColor, Coord
+from .core import Coord, PlayerColor
 
-class BoardState:
-    def __init__(self, board: dict[Coord, PlayerColor], red_coordinates: list[Coord], parent = None, redInserted = None,cost = 0):
-        self.board = board
-        self.red_coordinates = red_coordinates
-        self.parent = parent
-        self.redInserted = redInserted
-        self.cost = cost
-
-
-    def to_tuple(self) -> tuple[dict[Coord, PlayerColor], list[Coord]]:
-        return self.board, self.red_coordinates
-
-    @classmethod
-    def from_tuple(cls, state_tuple: tuple[dict[Coord, PlayerColor], list[Coord]]):
-        return cls(*state_tuple)
-    def __lt__(self, other):
-        return len(self.red_coordinates) < len(other.red_coordinates)
-    
+BLOCK_LEN = 4
 
 class Shape(Enum):
-    '''
-    An `Enum` class for all the different shape of tetrimino blocks
-    '''
     I1 = [(0,0), (1,0), (2,0), (3,0)]
     I2 = [(0,0), (0,1), (0,2), (0,3)]
 
@@ -49,3 +29,25 @@ class Shape(Enum):
 
     S1 = [(0,0),(0,1),(1,-1),(1,0)]
     S2 = [(0,0),(1,0),(1,1),(2,1)]
+
+class BoardState:
+    def __init__(self, board: dict[Coord, PlayerColor], parent = None, redInserted = None,cost = 0,redCoord = None,
+                 bestR = 11, bestC = 11):
+        self.board = board
+        #self.red_coordinates = red_coordinates
+        self.parent = parent
+        self.redInserted = redInserted
+        self.cost = cost
+        self.redCoord = redCoord
+        self.bestRow = bestR
+        self.bestCol = bestC
+
+
+    def to_tuple(self) -> tuple[dict[Coord, PlayerColor], list[Coord]]:
+        return self.board, self.red_coordinates
+
+    @classmethod
+    def from_tuple(cls, state_tuple: tuple[dict[Coord, PlayerColor], list[Coord]]):
+        return cls(*state_tuple)
+    def __lt__(self, other):
+        return self.cost < other.cost
